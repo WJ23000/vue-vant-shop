a9df6978c6511deab849de0bf4ad2cc6121b8596
Wj-Mr <percy7@yeah.net>
2019-02-19 20:03:57 +0800

    评论列表使用vuex状态管理

--- src/store/store.js
+++ src/store/store.js
@@ -5,14 +5,37 @@ Vue.use(Vuex);
 let store = new Vuex.Store({
     // 1. state
     state:{
-        collectionList:"城市名"
+        evaluList:[
+            {
+                id: 1,
+                name: "乐购商城",
+                title: '智能飞行器高空拍摄续航6小时，高清在线生成远景视频，蓝色荣耀奢华版',
+                price: 3500.00,
+                static: "已评价",
+                specif: "蓝色",
+                count: 2,
+                url: '',
+                icon: require('../assets/shop1.png')
+            },
+            {
+                id: 2,
+                name: "乐购商城",
+                title: '智能飞行器高空拍摄续航6小时，高清在线生成远景视频，蓝色荣耀奢华版',
+                price: 3500.00,
+                static: "已评价",
+                specif: "蓝色",
+                count: 2,
+                url: '',
+                icon: require('../assets/shop1.png')
+            }
+        ]
     },
 
     // 2. getters(此处方法给其他页面提供用户数据)
     getters:{
         // 参数列表state指的是state数据
-        getUserFn(state){
-            return state.user;
+        getEvaluListFn(state){
+            return state.evaluList;
         }
     },
     // 3. actions
@@ -23,18 +46,18 @@ let store = new Vuex.Store({
         // state指的是state数据
         // commit调用mutations的方法 
         // name就是调用此方法时要传的参数
-        setUserName({commit,state}, name){
+        setEvaluListFn({commit,state}, name){
             // 跟后台打交道
             // 调用mutaions里面的方法
-            commit("setUser", name);
+            commit("setEvaluList", name);
         }
     },
     // 4. mutations
     mutations:{
         // state指的是state的数据
         // name传递过来的数据
-        setUser(state, name){
-            state.user = name;//将传参设置给state的user
+        setEvaluList(state, name){
+            state.evaluList = name;//将传参设置给state的user
         }
     }
 });
