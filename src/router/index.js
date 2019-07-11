429cc5d03854e64a7a7bbfb743d3f798e45994a4
Wj-Mr <percy7@yeah.net>
2019-07-11 23:49:57 +0800

    路由使用scrollBehavior属性解决滚动条行为

--- src/router/index.js
+++ src/router/index.js
@@ -26,122 +26,137 @@ import BindPhone from '@/pages/BindPhone'
 
 Vue.use(Router)
 
-export default new Router({
-  //mode: 'history',开发时使用
-  routes: [
-    {
-      path: '/',
-      name: 'Index',
-      meta: { index: 0 },//meta对象的index用来定义当前路由的层级,由小到大,由低到高
-      component: Index
-    },
-    {
-      path: '/Login',
-      name: 'Login',
-      meta: { index: 0 },//meta对象的index用来定义当前路由的层级,由小到大,由低到高
-      component: Login
-    },
-    {
-      path: '/Class',
-      name: 'Class',
-      meta: { index: 0 },//meta对象的index用来定义当前路由的层级,由小到大,由低到高
-      component: Class
-    },
-    {
-      path: '/Cart',
-      name: 'Cart',
-      meta: { index: 0 },//meta对象的index用来定义当前路由的层级,由小到大,由低到高
-      component: Cart
-    },
-    {
-      path: '/Mine',
-      name: 'Mine',
-      meta: { index: 0 },//meta对象的index用来定义当前路由的层级,由小到大,由低到高
-      component: Mine
-    },
-    {
-      path: '/OrderList',
-      name: 'OrderList',
-      meta: { index: 1 },//meta对象的index用来定义当前路由的层级,由小到大,由低到高
-      component: OrderList
-    },
-    {
-      path: '/OrderDetail',
-      name: 'OrderDetail',
-      meta: { index: 2 },//meta对象的index用来定义当前路由的层级,由小到大,由低到高
-      component: OrderDetail
-    },
-    {
-      path: '/EvaluList',
-      name: 'EvaluList',
-      meta: { index: 1 },//meta对象的index用来定义当前路由的层级,由小到大,由低到高
-      component: EvaluList
-    },
-    {
-      path: '/Evalu',
-      name: 'Evalu',
-      meta: { index: 2 },//meta对象的index用来定义当前路由的层级,由小到大,由低到高
-      component: Evalu
-    },
-    {
-      path: '/Address',
-      name: 'Address',
-      meta: { index: 1 },//meta对象的index用来定义当前路由的层级,由小到大,由低到高
-      component: Address
-    },
-    {
-      path: '/AddressAdd',
-      name: 'AddressAdd',
-      meta: { index: 2 },//meta对象的index用来定义当前路由的层级,由小到大,由低到高
-      component: AddressAdd
-    },
-    {
-      path: '/Collection',
-      name: 'Collection',
-      meta: { index: 1 },//meta对象的index用来定义当前路由的层级,由小到大,由低到高
-      component: Collection
-    },
-    {
-      path: '/Set',
-      name: 'Set',
-      meta: { index: 1 },//meta对象的index用来定义当前路由的层级,由小到大,由低到高
-      component: Set
-    },
-    {
-      path: '/BindPhone',
-      name: 'BindPhone',
-      meta: { index: 3 },//meta对象的index用来定义当前路由的层级,由小到大,由低到高
-      component: BindPhone
-    },
-    {
-      path: '/Search',
-      name: 'Search',
-      meta: { index: 1 },//meta对象的index用来定义当前路由的层级,由小到大,由低到高
-      component: Search
-    },
-    {
-      path: '/SearchOrderList',
-      name: 'SearchOrderList',
-      meta: { index: 1 },//meta对象的index用来定义当前路由的层级,由小到大,由低到高
-      component: SearchOrderList
-    },
-    {
-      path: '/GoodsDetail',
-      name: 'GoodsDetail',
-      meta: { index: 2 },//meta对象的index用来定义当前路由的层级,由小到大,由低到高
-      component: GoodsDetail
-    },
-    {
-      path: '/PayOrder',
-      name: 'PayOrder',
-      meta: { index: 2 },//meta对象的index用来定义当前路由的层级,由小到大,由低到高
-      component: PayOrder
-    },
-    {
-      path: '/Comment',
-      name: 'Comment',
-      meta: { index: 3 },//meta对象的index用来定义当前路由的层级,由小到大,由低到高
-      component: Comment
-    }
-  ]
+
+export const constantRoutes = [
+  {
+    path: '/',
+    name: 'Index',
+    meta: { index: 0 },//meta对象的index用来定义当前路由的层级,由小到大,由低到高
+    component: Index
+  },
+  {
+    path: '/Login',
+    name: 'Login',
+    meta: { index: 0 },//meta对象的index用来定义当前路由的层级,由小到大,由低到高
+    component: Login
+  },
+  {
+    path: '/Class',
+    name: 'Class',
+    meta: { index: 0 },//meta对象的index用来定义当前路由的层级,由小到大,由低到高
+    component: Class
+  },
+  {
+    path: '/Cart',
+    name: 'Cart',
+    meta: { index: 0 },//meta对象的index用来定义当前路由的层级,由小到大,由低到高
+    component: Cart
+  },
+  {
+    path: '/Mine',
+    name: 'Mine',
+    meta: { index: 0 },//meta对象的index用来定义当前路由的层级,由小到大,由低到高
+    component: Mine
+  },
+  {
+    path: '/OrderList',
+    name: 'OrderList',
+    meta: { index: 1 },//meta对象的index用来定义当前路由的层级,由小到大,由低到高
+    component: OrderList
+  },
+  {
+    path: '/OrderDetail',
+    name: 'OrderDetail',
+    meta: { index: 2 },//meta对象的index用来定义当前路由的层级,由小到大,由低到高
+    component: OrderDetail
+  },
+  {
+    path: '/EvaluList',
+    name: 'EvaluList',
+    meta: { index: 1 },//meta对象的index用来定义当前路由的层级,由小到大,由低到高
+    component: EvaluList
+  },
+  {
+    path: '/Evalu',
+    name: 'Evalu',
+    meta: { index: 2 },//meta对象的index用来定义当前路由的层级,由小到大,由低到高
+    component: Evalu
+  },
+  {
+    path: '/Address',
+    name: 'Address',
+    meta: { index: 1 },//meta对象的index用来定义当前路由的层级,由小到大,由低到高
+    component: Address
+  },
+  {
+    path: '/AddressAdd',
+    name: 'AddressAdd',
+    meta: { index: 2 },//meta对象的index用来定义当前路由的层级,由小到大,由低到高
+    component: AddressAdd
+  },
+  {
+    path: '/Collection',
+    name: 'Collection',
+    meta: { index: 1 },//meta对象的index用来定义当前路由的层级,由小到大,由低到高
+    component: Collection
+  },
+  {
+    path: '/Set',
+    name: 'Set',
+    meta: { index: 1 },//meta对象的index用来定义当前路由的层级,由小到大,由低到高
+    component: Set
+  },
+  {
+    path: '/BindPhone',
+    name: 'BindPhone',
+    meta: { index: 3 },//meta对象的index用来定义当前路由的层级,由小到大,由低到高
+    component: BindPhone
+  },
+  {
+    path: '/Search',
+    name: 'Search',
+    meta: { index: 1 },//meta对象的index用来定义当前路由的层级,由小到大,由低到高
+    component: Search
+  },
+  {
+    path: '/SearchOrderList',
+    name: 'SearchOrderList',
+    meta: { index: 1 },//meta对象的index用来定义当前路由的层级,由小到大,由低到高
+    component: SearchOrderList
+  },
+  {
+    path: '/GoodsDetail',
+    name: 'GoodsDetail',
+    meta: { index: 2 },//meta对象的index用来定义当前路由的层级,由小到大,由低到高
+    component: GoodsDetail
+  },
+  {
+    path: '/PayOrder',
+    name: 'PayOrder',
+    meta: { index: 2 },//meta对象的index用来定义当前路由的层级,由小到大,由低到高
+    component: PayOrder
+  },
+  {
+    path: '/Comment',
+    name: 'Comment',
+    meta: { index: 3 },//meta对象的index用来定义当前路由的层级,由小到大,由低到高
+    component: Comment
+  }
+]
+
+
+const createRouter = () => new Router({
+  // mode: 'history', // require service support
+  scrollBehavior: () => ({ y: 0 }),
+  routes: constantRoutes
 })
+
+const router = createRouter()
+
+// Detail see: https://github.com/vuejs/vue-router/issues/1234#issuecomment-357941465
+export function resetRouter() {
+  const newRouter = createRouter()
+  router.matcher = newRouter.matcher // reset router
+}
+
+export default router
