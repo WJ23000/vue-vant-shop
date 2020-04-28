0f09d5112c5d6870f94e90f272f47dc2902cd396
Wj-Mr <percy7@yeah.net>
2020-04-28 22:13:28 +0800

    初始化baseUrl

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,6 @@
 const href = window.location.href;
 
-let baseUrl = "";
+let baseUrl = "https://mock.yonyoucloud.com/mock/6119";
 if (/(\.test\.)/.test(href)) {
     baseUrl = '';
 } else if (/localhost|\.dev\.|((25[0-5]|2[0-4]\d|((1\d{2})|([1-9]?\d)))\.){3}(25[0-5]|2[0-4]\d|((1\d{2})|([1-9]?\d)))/.test(href)) {
